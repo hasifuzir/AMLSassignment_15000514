f24b121538da1f10ce62d04343ee5ecaaa0854f9
Hasif Uzir <hasifuzir@gmail.com>
2018-12-16 19:20:51 +0000

    Added verbosity

--- preprocess.py
+++ preprocess.py
@@ -9,12 +9,12 @@ import cv2
 import shutil
 
 # Set paths
-global base_dir
 base_dir = './dataset'
 images_dir = os.path.join(base_dir,'images')
 labels_file = os.path.join(base_dir,'attribute_list.csv')
 test_dest = os.path.join(base_dir,'test')
 train_dest = os.path.join(base_dir,'train')
+
 
 # Removes noise images from the labels
 def remove_noise():
@@ -34,28 +34,38 @@ def split_images_binary(df, split):
 
     if os.path.exists(train_dest):
         shutil.rmtree(train_dest)
+        print("Deleted previous Train folder!")
         os.mkdir(train_dest)
+        print("Created Train folder!")
 
     else:
         os.mkdir(train_dest)
+        print("Created Train folder!")
 
     if os.path.exists(test_dest):
         shutil.rmtree(test_dest)
+        print("Deleted previous Test folder!")
         os.mkdir(test_dest)
+        print("Created Test folder!")
 
     else:
         os.mkdir(test_dest)
+        print("Created Test folder!")
 
     for index, rows in train.iterrows():
         img = cv2.imread(images_dir + "/" + str(rows['file_name']) + ".png")
         cv2.imwrite(train_dest + "/" + str(rows['file_name']) + ".png", img)
 
+    print("Copied training images into Train folder!")
+
     for index, rows in test.iterrows():
         img = cv2.imread(images_dir + "/" + str(rows['file_name']) + ".png")
         cv2.imwrite(test_dest + "/" + str(rows['file_name']) + ".png", img)
+
+    print("Copied testing images into Test folder!")
 
     return train, test, train_filename, test_filename
 
 def extract_features():
 
-    return
+    return 0
