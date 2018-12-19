7afdf35d9cba4b0412545a153b65e88444e642c1
Hasif Uzir <hasifuzir@gmail.com>
2018-12-19 10:14:12 +0000

    Merged binary and multiclass classification fn

--- classification.py
+++ classification.py
@@ -4,93 +4,119 @@ from keras.layers import Conv2D #For convoluting images (2D arrays)
 from keras.layers import MaxPooling2D #To use Maxpooling function
 from keras.layers import Flatten #To convert 2D arrays to a single linear vect
 from keras.layers import Dense #To perform NN connections
-from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dropout #For regularization
+import keras.callbacks as callbacks #To use TensorBoard
+from keras.preprocessing.image import ImageDataGenerator #To augment images
 from PIL import Image
 import numpy as np
 import pandas as pd
-import sklearn.metrics as metrics
-import matplotlib.pyplot as plt
 
-def train_CNN_binary(train_df, test_df, label_col, train_dest, test_dest, weights):
+# Obtain training and testing directory paths
+def get_paths(train_directory, test_directory):
+    global train_dest
+    global test_dest
+
+    train_dest = train_directory
+    test_dest = test_directory
+
+    return 0
+
+# A CNN trained to produce binary outputs
+# Takes training and testing dataframes and their respective image directories plus class weights
+# Trains, evaluates and tests a CNN as well as produce plots and a results csv file
+def train_CNN(train_df, test_df, label_col, batch, weights, mode):
+    if mode == 0:
+        output = 1
+        final_activation = 'sigmoid'
+        optimizer_mode = 'adam'
+        loss_mode = 'binary_crossentropy'
+        metrics_mode = 'binary_accuracy'
+        flow_mode = "binary"
+
+    else:
+        output = 7
+        final_activation = 'softmax'
+        optimizer_mode = 'rmsprop'
+        loss_mode = 'categorical_crossentropy'
+        metrics_mode = 'categorical_accuracy'
+        flow_mode = "categorical"
+
     classifier = Sequential() #Create Sequential object
 
-    # Convolution
-    # Filters: 32
-    # Filter shape: 3X3
-    # Input shape: 256X256, RGB(3)
-    # Activation function: Rectifier
+    # Convolution layer with Non Linearity
+    # To extract features from input
+    # Filters: 32, Filter shape: 3X3, Input shape: 256X256, RGB(3), Activation function: Rectifier Linear Unit
     classifier.add(Conv2D(32, (3,3), input_shape = (256, 256, 3), activation = 'relu'))
 
-    # Pooling
+    # Pooling layer using Max Pooling
     # To reduce the size of images and total number of nodes
     classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
+    #More convolution and pooling layers
     classifier.add(Conv2D(32, (3,3), activation = 'relu'))
     classifier.add(MaxPooling2D(pool_size=(2, 2)))
-
+    classifier.add(Dropout(0.25))
     classifier.add(Conv2D(64, (3,3), activation = 'relu'))
     classifier.add(MaxPooling2D(pool_size=(2, 2)))
+    classifier.add(Dropout(0.25))
 
     # Flattening
     # Convert 3D feature map to 1D feature vector
     classifier.add(Flatten())
 
-    # Hidden layer
-    # Nodes: 128
-    # Activation function: Rectifier
+    # Fully Connected/ Hidden layer
+    # A fully connected layer
+    # Nodes: 128, Activation function: Rectifier Linear Unit
     classifier.add(Dense(units = 128, activation = 'relu'))
-
+    classifier.add(Dropout(0.25))
     # Output layer
     # One node for binary output
     # Activation function: Sigmoid
-    classifier.add(Dense(units = 1, activation = 'sigmoid'))
+    classifier.add(Dense(units = output, activation = final_activation))
 
     # Compile CNN
-    # Optimiser parameter: adam
-    # Loss function
-    # Performance metric
-    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['binary_accuracy'])
+    # Optimiser parameter: adam, Loss function: Binary Crossentropy, Performance metric: Binary Accuracy
+    classifier.compile(optimizer = optimizer_mode, loss = loss_mode, metrics = [metrics_mode])
 
+    #Realtime image augmentation
     train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True, validation_split = 0.25)
     test_datagen = ImageDataGenerator(rescale = 1./255)
-    train_generator = train_datagen.flow_from_dataframe(dataframe = train_df, directory = train_dest, x_col = "file_name", y_col = label_col, has_ext = False, batch_size = 32, seed = 42, shuffle = True, class_mode = "binary", target_size = (256, 256), subset = "training")
-    valid_generator = train_datagen.flow_from_dataframe(dataframe = train_df, directory = train_dest, x_col = "file_name", y_col = label_col, has_ext = False, batch_size = 32, seed = 42, shuffle = True, class_mode = "binary", target_size = (256, 256), subset = "validation")
+
+    #Takes dataframe input and images directory and generate batches of augmented data
+    train_generator = train_datagen.flow_from_dataframe(dataframe = train_df, directory = train_dest, x_col = "file_name", y_col = label_col, has_ext = False, batch_size = batch, seed = 42, shuffle = True, class_mode = flow_mode, target_size = (256, 256), subset = "training")
+    valid_generator = train_datagen.flow_from_dataframe(dataframe = train_df, directory = train_dest, x_col = "file_name", y_col = label_col, has_ext = False, batch_size = batch, seed = 42, shuffle = True, class_mode = flow_mode, target_size = (256, 256), subset = "validation")
     test_generator = test_datagen.flow_from_dataframe(dataframe = test_df, directory = test_dest, x_col = "file_name", y_col = None, has_ext = False, batch_size = 1, seed = 42, shuffle = False, class_mode = None, target_size = (256, 256)) #Batch size needs to be divisible by 913
 
+    #Obtain steps required for training, evaluating and testing
     step_size_train = train_generator.n//train_generator.batch_size
     step_size_valid = valid_generator.n//valid_generator.batch_size
     step_size_test = test_generator.n//test_generator.batch_size
 
-    train_history = classifier.fit_generator(train_generator, steps_per_epoch = step_size_train, validation_data = valid_generator, validation_steps = step_size_valid, epochs = 10, class_weight = weights, verbose = 1)
+    #Callbacks
+    tensorboard = callbacks.TensorBoard(log_dir='./logs', histogram_freq = 0, write_graph = True, write_images = True)
+    csv_logger = callbacks.CSVLogger('./logs/' + label_col + '_training_log.csv', separator=',', append=False)
 
-    # Evaluate model
+    #Train model on generated data via batches
+    train_history = classifier.fit_generator(train_generator, steps_per_epoch = step_size_train, validation_data = valid_generator, validation_steps = step_size_valid, epochs = 20, class_weight = weights, verbose = 1, callbacks = [tensorboard, csv_logger], workers = 8)
+
+    # Evaluate model and assign loss and accuracy scores
     score = classifier.evaluate_generator(generator = valid_generator, steps = step_size_valid, verbose = 1)
-    print('Test loss:', score[0])
-    print('Test accuracy:', score[1])
-
-    loss = train_history.history['loss']
-    acc = train_history.history['binary_accuracy']
-    val_loss = train_history.history['val_loss']
-    val_acc = train_history.history['val_binary_accuracy']
-    plt.plot(loss)
-    plt.plot(val_loss)
-    plt.plot(acc)
-    plt.plot(val_acc)
-    plt.title('Model accuracy')
-    plt.legend(['loss', 'val_loss', 'binary_accuracy', 'val_binary_accuracy'])
-    plt.show()
 
     # Predict output
     test_generator.reset()
     prediction = classifier.predict_generator(test_generator, steps = step_size_test, verbose = 1) #Returns numpy array
 
-    #predicted_class_indices = np.argmax(prediction, axis = 1)
-    predicted_class_indices = [1 if x >= 0.5 else 0 for x in prediction]
-    labels = (train_generator.class_indices)
-    labels = dict((v,k) for k,v in labels.items())
-    predictions = [labels[k] for k in predicted_class_indices]
-    filenames = test_generator.filenames
-    results=pd.DataFrame({"Filename":filenames, "Predictions":predictions})
-    results.to_csv("results_" + label_col + ".csv", header = False, index = False)
+    #Create arrays of filenames and predictions
+    if mode == 0:
+        predicted_class_indices = [1 if x >= 0.5 else 0 for x in prediction]
+    else:
+        predicted_class_indices = np.argmax(prediction, axis = 1)
 
-    return 0
+    labels = (train_generator.class_indices) #Get the class label indices (so we know what 0 and 1 refers to)
+    print(labels)
+    labels = dict((v,k) for k,v in labels.items()) #Reverse indices
+    predictions = [labels[k] for k in predicted_class_indices] #Get all predictions (class) from dict
+    filenames = test_generator.filenames #Get all filenames of predictions
+    results = pd.DataFrame({"Filename":filenames, "Predictions":predictions})
+
+    return results, train_history, score
