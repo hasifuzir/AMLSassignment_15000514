810a751931aede2c2d937ee9c8a065b56d828e46
Hasif Uzir <hasifuzir@gmail.com>
2018-12-19 10:14:49 +0000

    Code cleanup to reflect new functions

--- main.py
+++ main.py
@@ -1,48 +1,64 @@
-# Task 1: Smile or NoSmile
 #==============================================================================
-# Import other python functions
+# Import required modules
 import preprocess as prep
 import classification as cls
+import analysis
 # Import required libraries
 import os
-import pandas as pd
-import tensorflow as tf
-from keras.preprocessing import image
-#from tensorflow import keras
-#import numpy as np
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-#from tensorflow.contrib import rnn
-#from tensorflow.python.ops import variable_scope
-#from tensorflow.python.framework import dtypes
-#import copy
 
-train_dest = prep.train_dest
-test_dest = prep.test_dest
+# Set paths
+base_dir = './dataset'
+images_dir = os.path.join(base_dir,'images')
+labels_file = os.path.join(base_dir,'attribute_list.csv')
+train_dest = os.path.join(base_dir,'train')
+test_dest = os.path.join(base_dir,'test')
+results_dir = "./results"
 
+# Set parameters
 # Based on dataset of 5000 images
-#train_num = 3750
-#val_num = 250
-#test_num = 1000
-#train_batch_size = 32
-test_split = 0.2 # 0.2 yields 3652/913 split
-# smiling, eyeglasess
-label_col = "human"
+batch_size = 32 #Size of batches for training and validation
+test_split = 0.2 #Ratio to split into training and testing images dataset, 0.2 yields 3652/913 split
+label_col = "smiling" #Class to train, validate and test on
 
+# Set custom class weights to compensate for imbalanced data
 def weights_set(label_col):
     weights = {
         "eyeglasses": {0: 2.438, 1: 1.},
         "smiling": {0: 1., 1: 3.903},
         "young": {0: 1., 1: 3.8},
-        "human": {0: 1.283, 1: 1.}
+        "human": {0: 1.283, 1: 1.},
+        "hair_color": {0: 1., 1: 1., 2: 1., 3: 1., 4: 1., 5: 1., 6: 1.}
     }
 
+# Pass paths to modules that require them
+prep.get_paths(images_dir, train_dest, test_dest)
+cls.get_paths(train_dest, test_dest)
+analysis.get_paths(results_dir)
+
+# Set whether binary or multiclass prediction
+if label_col == "hair_color":
+    pred_mode = 1
+else:
+    pred_mode = 0
+
 # Obtain filtered data
-data = prep.remove_noise()
-#print(data) #DEBUG
+data = prep.remove_noise(labels_file)
 
-# Obtain training and testing datasets
-train_df, test_df, train_f, test_f = prep.split_images_binary(data, test_split)
-test_df.to_csv("answers_" + label_col + ".csv", index = False)
+# Obtain training and testing datasets and save testing answers as csv file
+train_df, test_df = prep.split_images_binary(data, test_split)
+prep.save_answers(test_df, label_col)
 
-cls.train_CNN_binary(train_df, test_df, label_col, train_dest, test_dest, weights_set(label_col))
+# Train CNN and obtain results csv, keras training history and metric scores
+results, train_history, score = cls.train_CNN(train_df, test_df, label_col, batch_size, weights_set(label_col), pred_mode)
+
+# Print Test loss and accuracy
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# Plot graphs based on loss and accuracy for training and validation
+analysis.plot_graph(train_history, label_col, pred_mode)
+
+# Save predriction results to csv
+analysis.save_pred(results, label_col)
+
+#Pass prediction dataframes so we can plot graphs and stuff?
