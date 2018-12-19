7fdaee100efe046d213e7ffbe2895e576eac278b
Hasif Uzir <hasifuzir@gmail.com>
2018-12-19 10:14:31 +0000

    Create fn to save answers and get paths

--- preprocess.py
+++ preprocess.py
@@ -1,24 +1,26 @@
 # Import required libraries
 import os
 import pandas as pd
-import numpy as np
-import matplotlib as plt
-from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 import cv2
 import shutil
 
-# Set paths
-base_dir = './dataset'
-images_dir = os.path.join(base_dir,'images')
-labels_file = os.path.join(base_dir,'attribute_list.csv')
-test_dest = os.path.join(base_dir,'test')
-train_dest = os.path.join(base_dir,'train')
+#Get base, images and label CSV paths from main
+def get_paths(images_directory, train_directory, test_directory):
+    global images_dir
+    global train_dest
+    global test_dest
+
+    images_dir = images_directory
+    train_dest = train_directory
+    test_dest = test_directory
+
+    return 0
 
 # Removes noise images from the labels
-def remove_noise():
+def remove_noise(labels_file):
     # Read dataset
-    df = pd.read_csv(labels_file, skiprows=1) #Skip the initial row
+    df = pd.read_csv(labels_file, skiprows = 1) #Skip the initial row (classes)
     # Available features: file_name,hair_color,eyeglasses,smiling,young,human
     # Filer out noise (All noise has a value of -1 across all features)
     df_filtered = df.loc[~((df['hair_color'] == -1) & (df['eyeglasses'] == -1) & (df['smiling'] == -1) & (df['young'] == -1) & (df['human'] == -1))]
@@ -28,8 +30,6 @@ def remove_noise():
 # Splits images dataset into train and validation directories
 def split_images_binary(df, split):
     train, test = train_test_split(df, test_size = split)
-    train_filename = train.loc[:, 'file_name']
-    test_filename = test.loc[:, 'file_name']
 
     if os.path.exists(train_dest):
         shutil.rmtree(train_dest)
@@ -63,8 +63,11 @@ def split_images_binary(df, split):
 
     print("Copied testing images into Test folder!")
 
-    return train, test, train_filename, test_filename
+    return train, test
 
-def extract_features():
+# Save test answers to csv
+def save_answers(test_df, label_col):
+    test_df.to_csv("answers_" + label_col + ".csv", index = False)
+    print("Saved answer file to answers_" + label_col + ".csv !")
 
     return 0
