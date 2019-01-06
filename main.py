726d539086bd141278a8404964733822adc11aa5
Hasif Uzir <hasifuzir@gmail.com>
2019-01-06 23:00:36 +0000

    Cleanup and comments

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#==============================================================================
 # Import required modules
 import preprocess as prep
 import classification as cls
@@ -20,7 +19,7 @@ batch_size = 32 #Size of batches for training and validation
 test_split = 0.2 #Ratio to split into training and testing images dataset, 0.2 yields 3652/913 split
 label_col = 'eyeglasses'
  #Class to train, validate and test on
-#0 for manual class_weights, 1 for auto set, 2 for auto set with NaN suppression (multiclass only!) , 3 for equal weights
+ #auto, manual, equal, nan_suppress (only for multiclass!)
 weights_mode = 'auto'
 
 # Function to set get custom weight dict based on class
@@ -28,14 +27,12 @@ def manual_weights_set(label_col):
     return {
         # weight_index: dataset_index
         # Binary classes
-        # 0: -1, 1: 1
         "smiling": {0: 3.90333, 1: 1.},
         "young": {0: 3.80021, 1: 1.},
         "eyeglasses": {0: 1., 1: 2.4375},
         "human": {0: 1., 1: 1.2825},
 
         # Multiclass classes
-        # 0: -1, 1: 0, 2: 1, 3: 2, 4: 3, 5: 5, 6: 5
         "hair_color": {0: 1.5, 1: 11.30682, 2: 1., 3: 1.819013, 4: 1.055143, 5: 1.26269, 6: 1.839187}
     }.get(label_col, 'auto') #rturns proper class weight, else returns auto which will let sklearn to calculate it automatically
 
@@ -83,5 +80,3 @@ analysis.plot_graph(train_history, label_col, pred_mode)
 
 # Save prediction results to csv
 analysis.save_pred(results, label_col, score[1])
-
-#Pass prediction dataframes so we can plot graphs and stuff?
