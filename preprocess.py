830467efb0f7755f97577022f02795adcb0f423d
Hasif Uzir <hasifuzir@gmail.com>
2019-01-06 23:00:42 +0000

    Cleanup and comments

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,9 @@
 # Import required libraries
 import os
 import pandas as pd
-from sklearn.model_selection import train_test_split
-import cv2
-import shutil
+from sklearn.model_selection import train_test_split #For train and test dataset splitting
+import cv2 #For copying images
+import shutil #To delete folders
 
 #Get base, images and label CSV paths from main
 def get_paths(images_directory, train_directory, test_directory):
@@ -14,7 +14,7 @@ def get_paths(images_directory, train_directory, test_directory):
     images_dir = images_directory
     train_dest = train_directory
     test_dest = test_directory
-
+    
     return 0
 
 # Removes noise images from the labels
@@ -24,43 +24,41 @@ def remove_noise(labels_file):
     # Available features: file_name,hair_color,eyeglasses,smiling,young,human
     # Filer out noise (All noise has a value of -1 across all features)
     df_filtered = df.loc[~((df['hair_color'] == -1) & (df['eyeglasses'] == -1) & (df['smiling'] == -1) & (df['young'] == -1) & (df['human'] == -1))]
-
     return df_filtered
 
 # Splits images dataset into train and validation directories
 def split_images_binary(df, split):
     train, test = train_test_split(df, test_size = split)
 
+    #Create the Train folder
     if os.path.exists(train_dest):
         shutil.rmtree(train_dest)
         print("Deleted previous Train folder!")
         os.mkdir(train_dest)
         print("Created Train folder!")
-
     else:
         os.mkdir(train_dest)
         print("Created Train folder!")
 
+    #Create the Test folder
     if os.path.exists(test_dest):
         shutil.rmtree(test_dest)
         print("Deleted previous Test folder!")
         os.mkdir(test_dest)
         print("Created Test folder!")
-
     else:
         os.mkdir(test_dest)
         print("Created Test folder!")
 
+    #Copy images from dataset to the new folders
     for index, rows in train.iterrows():
         img = cv2.imread(images_dir + "/" + str(rows['file_name']) + ".png")
         cv2.imwrite(train_dest + "/" + str(rows['file_name']) + ".png", img)
-
     print("Copied training images into Train folder!")
 
     for index, rows in test.iterrows():
         img = cv2.imread(images_dir + "/" + str(rows['file_name']) + ".png")
         cv2.imwrite(test_dest + "/" + str(rows['file_name']) + ".png", img)
-
     print("Copied testing images into Test folder!")
 
     return train, test
